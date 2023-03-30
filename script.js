d0b02934ccfa99617cd2d1e5d8cfb1f0f830d240
Jishan040 <118433367+Jishan040@users.noreply.github.com>
2023-03-31 01:47:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -43,25 +43,3 @@ function getElementsByAttribute(attribute, value) {
 }
  const elementsWithBarBaz = getElementsByAttribute("data-bar", "baz");
     console.log(elementsWithBarBaz);
-
-  makeSound() {
-    console.log("The " + this.species + " makes a sound");
-  }
-}
-
-class Cat extends Animal {
-  purr() {
-    console.log("purr");
-  }
-}
-
-class Dog extends Animal {
-  bark() {
-    console.log("woof");
-  }
-}
-
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
