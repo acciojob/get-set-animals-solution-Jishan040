b3050f7c658c9abb1926728ae79f17a4cdd76ff7
Jishan040 <118433367+Jishan040@users.noreply.github.com>
2023-03-31 01:42:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -28,3 +28,40 @@ class Dog extends Animal {
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
+
+function getElementsByAttribute(attribute, value) {
+  const elementsWithAttribute = [];
+  const allElements = document.getElementsByTagName("*");
+  
+  for (let i = 0; i < allElements.length; i++) {
+    if (allElements[i].getAttribute(attribute) === value) {
+      elementsWithAttribute.push(allElements[i]);
+    }
+  }
+  
+  return elementsWithAttribute;
+}
+ const elementsWithBarBaz = getElementsByAttribute("data-bar", "baz");
+    console.log(elementsWithBarBaz);
+
+  makeSound() {
+    console.log("The " + this.species + " makes a sound");
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Do not change the code below this line
+window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
